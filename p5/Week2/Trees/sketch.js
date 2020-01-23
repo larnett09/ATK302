71468a1ce56f01cabbf7d629752b6b857d116b80
llo_0py <larnett.br@outlook.com>
2020-01-23 10:21:44 -0600

    Beautiful now

--- p5/Week2/Trees/sketch.js
+++ p5/Week2/Trees/sketch.js
@@ -5,45 +5,45 @@ function setup() {
   noStroke();
 
 
-//trunks of junk
+  //trunks of junk
 
-	fill('#663300');
-	rectMode(CENTER);
-	rect(100, 280, 20, 280);
+  fill('#663300');
+  rectMode(CENTER);
+  rect(100, 280, 20, 280);
 
-	fill('#663300');
-	rectMode(CENTER);
-	rect(300, 280, 20, 280);
+  fill('#663300');
+  rectMode(CENTER);
+  rect(300, 280, 20, 280);
 
-	fill('#663300');
-	rectMode(CENTER);
-	rect(500, 280, 20, 280);
+  fill('#663300');
+  rectMode(CENTER);
+  rect(500, 280, 20, 280);
 
-	//start of arcs for hills
+  //start of arcs for hills
 
-	fill('#009933');
+  fill('#009933');
   arc(100, 400, 280, 280, PI, TWO_PI);
 
 
-	fill('#003300');
+  fill('#003300');
   arc(300, 400, 280, 280, PI, TWO_PI);
 
 
-	fill('#33cc33');
+  fill('#33cc33');
   arc(500, 400, 280, 280, PI, TWO_PI);
 
-	// second smaller set of arcs
+  // second smaller set of arcs
 
-	fill('#33cc33');
+  fill('#33cc33');
   arc(100, 400, 200, 200, PI, TWO_PI);
 
-	fill('#009933');
+  fill('#009933');
   arc(300, 400, 200, 200, PI, TWO_PI);
 
-	fill('#003300');
+  fill('#003300');
   arc(500, 400, 200, 200, PI, TWO_PI);
 
-	//triangles for branches
+  //triangles for branches
 
   fill('#003300');
   triangle(100, 20, 50, 240, 150, 240);
@@ -51,7 +51,7 @@ function setup() {
   fill('#33cc33');
   triangle(300, 20, 250, 240, 350, 240);
 
-	fill('#009933');
+  fill('#009933');
   triangle(500, 20, 450, 240, 550, 240);
 
 }
