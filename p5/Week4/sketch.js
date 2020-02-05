c74cd38f93804573f99ddffdbc4b5e9e204d33f5
larnett09 <larnett.br@outlook.com>
2020-02-05 17:53:12 -0600

    trying other way

--- p5/Week4/sketch.js
+++ p5/Week4/sketch.js
@@ -6,7 +6,7 @@ function preload() {
 
 function setup() {
   createCanvas(800, 800);
-  tsurudapic = loadImage("assets/atk302tsuruda.jpg");
+//  tsurudapic = loadImage("assets/atk302tsuruda.jpg");
 }
 
 function draw() {
