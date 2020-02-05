8917abc95288f4586efbfe4065d34a26fec0a976
larnett09 <larnett.br@outlook.com>
2020-02-05 17:42:40 -0600

    ''

--- p5/Week4/sketch.js
+++ p5/Week4/sketch.js
@@ -1,33 +1,10 @@
-let img1;
-function preload() {
-  img1 = loadImage('assets/atk302tsuruda.jpg');
-}
+var tsurudapic ;
+
 function setup() {
-  background(50);
-  // Top-left corner of the img is at (10, 10)
-  // Width and height are 50 x 50
-  image(img, 10, 10, 100, 100);
+  createCanvas(800, 800) ;
+  tsurudapic = loadImage('assets/atk302tsuruda.jpg') ;
 }
 
-
-
-
-
-
-
-
-
-
-
-
-
-//var tsurudapic ;
-
-//function setup() {
-//  createCanvas(800, 800) ;
-//  tsurudapic = loadImage("assets/atk302tsuruda.jpg") ;
-//}
-
-//function draw() {
-//  image(tsurudapic, 20, 100) ;
-//}
+function draw() {
+  image(tsurudapic, 20, 100) ;
+}
