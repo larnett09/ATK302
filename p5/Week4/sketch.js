223a21564af06681b99695de82de34375b6ab4f9
larnett09 <larnett.br@outlook.com>
2020-02-05 20:23:13 -0600

    added fonts

--- p5/Week4/sketch.js
+++ p5/Week4/sketch.js
@@ -11,7 +11,7 @@ function setup() {
   tsurudapic = loadImage("assets/atk302tsuruda.jpg");
   flumepic = loadImage("assets/atk302flume.jpg");
   eprompic = loadImage("assets/atk302eprom.jpg");
-  tsurudafont = loadFont("assets/tsurudaf.otf");
+  tsurudafont = loadFont("assets/tsurudaf.ttf");
 }
 
 function draw() {
@@ -19,13 +19,13 @@ function draw() {
   image(flumepic, 210, 410, 300, 300);
   image(eprompic, 610, 210, 300, 300);
 
-  fill('#ED225D');   // SETS the color
-  textFont(tsurudafont);  // SETS the font
-  textSize(20);    // SETS the size of the font
-  text('TSURUDA', 10, 50); // DISPLAYS TEXT WITH THE FONT
+  fill('#ffffff');   // SETS the color
+ textFont(tsurudafont, 40);  // SETS the font
+ textSize();    // SETS the size of the font
+ text("TSURUDA", 20, 320); // DISPLAYS TEXT WITH THE FONT
 }
 
-}
+//}
 
 //console for mouse position
 //text(mouseX + ",  " + mouseY, mouseX + 10, mouseY + 10);
