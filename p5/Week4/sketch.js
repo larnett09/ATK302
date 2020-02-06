57e84146d27dfc091e8956ff2fa3381151f1f56c
larnett09 <larnett.br@outlook.com>
2020-02-06 12:46:52 -0600

    trry this! Added song to call back and set volume

--- p5/Week4/sketch.js
+++ p5/Week4/sketch.js
@@ -4,10 +4,10 @@ var eprompic;
 var tsurudafont;
 var epromfont;
 var flumefont;
-//var tsurudasong;
-//var flumesong;
-//var epromsong;
-
+var tsurudasong;
+var flumesong;
+var epromsong;
+var slider;
 
 function setup() {
   createCanvas(1200, 1200);
@@ -20,10 +20,15 @@ function setup() {
   epromfont = loadFont("assets/epromf.ttf");
   flumefont = loadFont("assets/flumef.ttf");
 
-  //tsurudasong = loadSong("assets/tsurudafubar.mp3");
-  //flumesong = loadSong("assets/flumeleftalone.mp3");
-  //epromsong = loadSong("assets/epromcenterofvip.mp3");
-  //flumesong.play();
+  tsurudasong = loadSound("assets/tsurudafubar.mp3");
+  //flumesong = loadSound("assets/flumeleftalone.mp3");
+  //epromsong = loadSound("assets/epromcenterofvip.mp3");
+  //slider = createSlider(300, 200, 0, 0.010);
+}
+
+function loaded() {
+  tsurudasong.play();
+  tsurudasong.setVolume(0.5);
 }
 
 function draw() {
@@ -31,24 +36,21 @@ function draw() {
   image(flumepic, 210, 410, 300, 300);
   image(eprompic, 610, 210, 300, 300);
 
-  fill('#ffffff'); // SETS the color
-  textFont(tsurudafont, 40); // SETS the font
-  text("TSURUDA", 20, 350); // DISPLAYS TEXT WITH THE FONT
+ fill('#ffffff');   // SETS the color
+ textFont(tsurudafont, 40);  // SETS the font
+ text("TSURUDA", 20, 350); // DISPLAYS TEXT WITH THE FONT
 
-  fill('#ffffff'); // SETS the color
-  textFont(epromfont, 40); // SETS the font
-  text("EPROM", 620, 550); // DISPLAYS TEXT WITH THE FONT
+ fill('#ffffff');   // SETS the color
+ textFont(epromfont, 40);  // SETS the font
+ text("EPROM", 620, 550); // DISPLAYS TEXT WITH THE FONT
 
-  fill('#ffffff'); // SETS the color
-  textFont(flumefont, 40); // SETS the font
-  text("FLUME", 220, 750); // DISPLAYS TEXT WITH THE FONT
+ fill('#ffffff');   // SETS the color
+ textFont(flumefont, 40);  // SETS the font
+ text("FLUME", 220, 750); // DISPLAYS TEXT WITH THE FONT
 }
 
-//function mouseReleased()
-//if (song1.isplaying())
-//song1.pause();
-//} else {
-// song1.play();
 //}
+
+//console for mouse position
+//text(mouseX + ",  " + mouseY, mouseX + 10, mouseY + 10);
 //}
-//
