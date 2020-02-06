af4c5dc87499f0a60759ea563826e4d2adc81ec3
larnett09 <larnett.br@outlook.com>
2020-02-05 20:04:27 -0600

    renamed

--- p5/Week4/sketch.js
+++ p5/Week4/sketch.js
@@ -5,14 +5,19 @@ var eprompic;
 //function preload() {}
 
 function setup() {
-  createCanvas(800, 800);
+  createCanvas(1200, 1200);
+  background("black")
   tsurudapic = loadImage("images/atk302tsuruda.jpg");
   flumepic = loadImage("images/atk302flume.jpg");
-  eprompic = loadImage("images.atk302eprom.jpg");
+  eprompic = loadImage("images/atk302eprom.jpg");
 }
 
 function draw() {
-  image(tsurudapic, 10, 10, 100, 100);
-  image(flumepic, 210, 210, 100, 100);
-  image(eprompic, 410, 410, 100, 100);
+  image(tsurudapic, 10, 10, 300, 300);
+  image(flumepic, 210, 410, 300, 300);
+  image(eprompic, 610, 210, 300, 300);
 }
+
+//console for mouse position
+//text(mouseX + ",  " + mouseY, mouseX + 10, mouseY + 10);
+//}
