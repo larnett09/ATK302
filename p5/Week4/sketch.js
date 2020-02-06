8d8d3e5df5ac100f148c698c65d1b516af41fc4f
llo_0py <larnett.br@outlook.com>
2020-02-06 10:28:16 -0600

    fix

--- p5/Week4/sketch.js
+++ p5/Week4/sketch.js
@@ -21,9 +21,9 @@ function setup() {
   epromfont = loadFont("assets/epromf.ttf");
   flumefont = loadFont("assets/flumef.ttf");
 
-tsurudasong = loadSong("assets/tsurudafubar.mp3");
-flumesong = loadSong("assets/flumeleftalone.mp3");
-epromsong = loadSong("assets/epromcenterofvip.mp3");
+  //tsurudasong = loadSong("assets/tsurudafubar.mp3");
+  //flumesong = loadSong("assets/flumeleftalone.mp3");
+  //epromsong = loadSong("assets/epromcenterofvip.mp3");
 }
 
 function draw() {
@@ -31,17 +31,17 @@ function draw() {
   image(flumepic, 210, 410, 300, 300);
   image(eprompic, 610, 210, 300, 300);
 
- fill('#ffffff');   // SETS the color
- textFont(tsurudafont, 40);  // SETS the font
- text("TSURUDA", 20, 350); // DISPLAYS TEXT WITH THE FONT
+  fill('#ffffff'); // SETS the color
+  textFont(tsurudafont, 40); // SETS the font
+  text("TSURUDA", 20, 350); // DISPLAYS TEXT WITH THE FONT
 
- fill('#ffffff');   // SETS the color
- textFont(epromfont, 40);  // SETS the font
- text("EPROM", 620, 550); // DISPLAYS TEXT WITH THE FONT
+  fill('#ffffff'); // SETS the color
+  textFont(epromfont, 40); // SETS the font
+  text("EPROM", 620, 550); // DISPLAYS TEXT WITH THE FONT
 
- fill('#ffffff');   // SETS the color
- textFont(flumefont, 40);  // SETS the font
- text("FLUME", 220, 750); // DISPLAYS TEXT WITH THE FONT
+  fill('#ffffff'); // SETS the color
+  textFont(flumefont, 40); // SETS the font
+  text("FLUME", 220, 750); // DISPLAYS TEXT WITH THE FONT
 }
 
 //}
