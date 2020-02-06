a96de011c0e196c06610f6545c04641835943e03
larnett09 <larnett.br@outlook.com>
2020-02-05 20:12:39 -0600

    try this

--- p5/Week4/sketch.js
+++ p5/Week4/sketch.js
@@ -1,21 +1,30 @@
 var tsurudapic;
 var flumepic;
 var eprompic;
+var tsurudafont;
 
 //function preload() {}
 
 function setup() {
   createCanvas(1200, 1200);
   background("black")
-  tsurudapic = loadImage("images/atk302tsuruda.jpg");
-  flumepic = loadImage("images/atk302flume.jpg");
-  eprompic = loadImage("images/atk302eprom.jpg");
+  tsurudapic = loadImage("assets/atk302tsuruda.jpg");
+  flumepic = loadImage("assets/atk302flume.jpg");
+  eprompic = loadImage("assets/atk302eprom.jpg");
+  tsurudafont = loadFont("assets/tsurudaf.otf");
 }
 
 function draw() {
   image(tsurudapic, 10, 10, 300, 300);
   image(flumepic, 210, 410, 300, 300);
   image(eprompic, 610, 210, 300, 300);
+
+  fill('#ED225D');   // SETS the color
+  textFont(tsurudafont);  // SETS the font
+  textSize(20);    // SETS the size of the font
+  text('TSURUDA', 10, 50); // DISPLAYS TEXT WITH THE FONT
+}
+
 }
 
 //console for mouse position
