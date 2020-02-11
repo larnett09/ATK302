8fe3bef9278bdc63f93219b5a821103aaf565028
llo_0py <larnett.br@outlook.com>
2020-02-11 11:18:07 -0600

    testing

--- p5/Week5/sketch.js
+++ p5/Week5/sketch.js
@@ -1,54 +1,30 @@
-var tsurudapic;
-var flumepic;
-var eprompic;
-var tsurudafont;
-var epromfont;
-var flumefont;
-//var tsurudasong;
-//var flumesong;
-//var epromsong;
-
+var myState = 0 ;
 
 function setup() {
-  createCanvas(1200, 1200);
-  background("black")
-  tsurudapic = loadImage("assets/atk302tsuruda.jpg");
-  flumepic = loadImage("assets/atk302flume.jpg");
-  eprompic = loadImage("assets/atk302eprom.jpg");
-
-  tsurudafont = loadFont("assets/tsurudaf.ttf");
-  epromfont = loadFont("assets/epromf.ttf");
-  flumefont = loadFont("assets/flumef.ttf");
-
-  //tsurudasong = loadSong("assets/tsurudafubar.mp3");
-  //flumesong = loadSong("assets/flumeleftalone.mp3");
-  //epromsong = loadSong("assets/epromcenterofvip.mp3");
-  //flumesong.play();
+  // put setup code here
+  createCanvas(800, 800) ;
+  //background()
 }
 
 function draw() {
-  image(tsurudapic, 10, 10, 300, 300);
-  image(flumepic, 210, 410, 300, 300);
-  image(eprompic, 610, 210, 300, 300);
+  // put drawing code here
+switch(myState){
+case 0:
+background('black')
+text("hello state 0!", 100, 100) ;
+break ;
 
-  fill('#ffffff'); // SETS the color
-  textFont(tsurudafont, 40); // SETS the font
-  text("TSURUDA", 20, 350); // DISPLAYS TEXT WITH THE FONT
+case 1:
+break ;
 
-  fill('#ffffff'); // SETS the color
-  textFont(epromfont, 40); // SETS the font
-  text("EPROM", 620, 550); // DISPLAYS TEXT WITH THE FONT
+case 2:
+break ;
 
-  fill('#ffffff'); // SETS the color
-  textFont(flumefont, 40); // SETS the font
-  text("FLUME", 220, 750); // DISPLAYS TEXT WITH THE FONT
+case 3:
+break ;
+
+case 4:
+break ;
 }
 
-//function mouseReleased()
-//if (song1.isplaying())
-//song1.pause();
-//} else {
-// song1.play();
-//}
-//}
-//
+}
