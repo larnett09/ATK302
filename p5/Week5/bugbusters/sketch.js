0e3ec76ce29bb57eb237e491fbe1b42d2333eff5
llo_0py <larnett.br@outlook.com>
2020-02-13 11:07:28 -0600

    made enouh fixes to try and set uo

---
+++ p5/Week5/bugbusters/sketch.js
@@ -0,0 +1,43 @@
+var state = 0;
+//var x;
+
+function Setup() {
+  // put setup code here
+  createCanvas(800, 800);
+}
+
+function Draw() {
+  switch (state) {
+    case 0:
+    Background(255, 0, 0);
+    fill('white');
+    textSize(100);
+    text('case 0', 2, 100);
+    break;
+
+    case 1:
+    background(0, 255, 0);
+    fill('black');
+    textSize(100);
+    text('case 1', 100, 100);
+    break;
+
+    case 2:
+    background(0, 0, 255);
+    fill('gold');
+    textSize(100);
+    text('case 2', 100, 100);
+    break;
+ }
+}
+
+function MouseRelesed() {
+  // add one to myState
+  // if myState got past 2, reset it to 0
+  state++ ;
+
+  if (state > 3) {
+      state = 0;
+  }
+
+}
