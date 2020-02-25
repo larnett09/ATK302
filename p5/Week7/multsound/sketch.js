d45050812c4b7242694a34172c8536877a372ca6
llo_0py <larnett.br@outlook.com>
2020-02-25 11:27:13 -0600

    changed

--- p5/Week7/multsound/sketch.js
+++ p5/Week7/multsound/sketch.js
@@ -1,11 +1,17 @@
-var = song1, song2, song3;
-var myState = 0;
+var song1, song2, song3;
+var myState = 0 ;
 
 function preload() {
   song1 = loadSound('assets/donkey.mp3');
   song2 = loadSound('assets/skate.mp3');
   song3 = loadSound('assets/thrill.mp3');
 
+  song1.loop();
+  song1.stop();
+  song2.loop();
+  song2.stop();
+  song3.loop();
+  song3.stop();
 }
 
 function setup() {
@@ -17,37 +23,37 @@ function setup() {
 }
 
 function draw() {
-  switch (myState) {
-    case 0:
-      song1.play();
-      myState = 1;
-      break;
+switch(myState) {
+  case 0:
+  song1.play();
+  myState = 1;
+  break ;
 
-    case 1:
-      break;
+  case 1:
+  break ;
 
-    case 2:
-      song2.play();
-      myState = 3;
-      break;
+  case 2:
+  song2.play ();
+  myState = 3;
+  break ;
 
-    case 3:
-      break;
+  case 3:
+  break ;
 
-    case 4:
-      song3.play();
-      myState = 5;
-      break;
+  case 4:
+  song3.play();
+  myState = 5;
+  break ;
 
-    case 5:
-      break;
+  case 5:
+  break ;
   }
 }
 
 function mousReleased() {
   myState = myState + 1;
-  if (myState > 5) {
-    myState = 0;
+  if (myState > 5){
+    myState = 0 ;
   }
 
   song1.pause();
@@ -57,6 +63,6 @@ function mousReleased() {
   getAudioContext().resume();
 }
 
-function touchStarted() {
+function touchStarted(){
   getAudioContext().resume();
 }
