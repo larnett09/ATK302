625d117161ed7ba4a472f7e1bd6b8f2af7df3461
larnett09 <larnett.br@outlook.com>
2020-02-29 16:36:15 -0600

    changed state info

--- p5/Week7/stoplight/sketch.js
+++ p5/Week7/stoplight/sketch.js
@@ -1,5 +1,5 @@
 var myState = 0;
-var timer = 5;
+var timer = 0;
 
 
 function setup() {
@@ -11,54 +11,57 @@ function setup() {
 
 function draw() {
   // put drawing code here
-  background(100);
+  background('white');
   fill('black');
   rect(width / 2, height / 2, 150, 400);
 
   switch (myState) {
-    case 0: // green state
-      fill('grey');
-      ellipse(width / 2, height / 2 - 125, 100, 100); // top circle
-      ellipse(width / 2, height / 2, 100, 100); // middle
+    case 0: // green
 
       fill('green');
-      ellipse(width / 2, height / 2 + 125, 100, 100); // bottom
-			timer++ ;
-      if (timer > 5) {
+      ellipse(width / 2, height / 2 + 125, 100, 100);
+
+      fill('white');
+      ellipse(width / 2, height / 2 - 125, 100, 100);
+      ellipse(width / 2, height / 2, 100, 100);
+
+      timer++;
+      if (timer > 250) {
         myState = 1;
         timer = 0;
       }
 
       break;
 
-    case 1: // yellow state      fill('grey');
-
-      fill('grey');
-      ellipse(width / 2, height / 2 - 125, 100, 100); // top circle
-      ellipse(width / 2, height / 2 + 125, 100, 100); // bottom
+    case 1: // yellow
 
       fill('yellow');
-      ellipse(width / 2, height / 2, 100, 100); // middle
-				timer++ ;
-	    if (timer > 5) {
+      ellipse(width / 2, height / 2, 100, 100);
+
+      fill('white');
+      ellipse(width / 2, height / 2 - 125, 100, 100);
+      ellipse(width / 2, height / 2 + 125, 100, 100);
+
+      timer++;
+      if (timer > 250) {
         myState = 2;
         timer = 0;
       }
 
       break;
 
-    case 2: // red state
+    case 2: // red
       fill('red');
-      ellipse(width / 2, height / 2 - 125, 100, 100); // top circle
+      ellipse(width / 2, height / 2 - 125, 100, 100);
 
-      fill('grey');
-      ellipse(width / 2, height / 2, 100, 100); // middle
-      ellipse(width / 2, height / 2 + 125, 100, 100); // bottom
+      fill('white');
+      ellipse(width / 2, height / 2, 100, 100);
+      ellipse(width / 2, height / 2 + 125, 100, 100);
 
       break;
-			timer++ ;
-      if (timer > 5) {
-        myState = 0;
+      timer--;
+      if (timer > 250) {
+        myState = 1;
         timer = 0;
       }
 
