b22198b9229ef6d41b2d6a1ad38a292804b9d745
larnett09 <larnett.br@outlook.com>
2020-03-05 08:28:09 -0600

    stuff

--- p5/Week8/MP2/sketch.js
+++ p5/Week8/MP2/sketch.js
@@ -1,17 +1,21 @@
 var myState = 0 ;
 var timer = 0 ;
 var kick;
+var clap;
 
 function preload(){
   //gif = loadGif('assets/giphybrain1.webp');
-  kick = loadSound('assets/kick1');
+  kick = loadSound('assets/kick1.wav');
   kick.loop();
   kick.stop();
+  clap.loop();
+  clap.stop();
 }
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
   background("white");
+  frameRate(1);
 }
 
 function draw() {
@@ -20,38 +24,40 @@ function draw() {
   switch(myState) {
     case 0:
     background(random(0,255),random(0,0),random(0,255));
-    fill('white')
-    text("This is your Brain on House", 500, 500);
+    fill('Black')
+    text("House music Is a feeling!", 500, 500);
     textAlign(CENTER, CENTER);
     textSize(28);
-    kick.play()
-
 
     timer++;
     if (timer > 250) {
       myState = 1;
       timer = 0;
-}
     break ;
 
     case 1:
-    background(random(0,255),random(0,0),random(0,255));
-    // put a timer in here -
-    // if timer counted up to 200, go to state 2 and reset timer!
-
-
     break ;
 
     case 2:
-    background(random(0,255),random(0,0),random(0,255));
+    //background(random(0,255),random(0,0),random(0,255));
+    background('black')
+    fill('white')
+    text("House music gets you dancing.", 500, 500);
+    textAlign(CENTER, CENTER);
+    textSize(28)
+    kick.play()
     break ;
 
     case 3:
-    background(random(0,255),random(0,0),random(0,255));
     break ;
 
     case 4:
     background(random(0,255),random(0,0),random(0,255));
+    fill('white')
+    text("House music Is a feeling!", 500, 500);
+    textAlign(CENTER, CENTER);
+    textSize(28)
+    clap.play()
     break ;
 
   }
