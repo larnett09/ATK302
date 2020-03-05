fb1d4c8878ad8a3f12af0ff716747d721cdd2f68
larnett09 <larnett.br@outlook.com>
2020-03-05 08:32:33 -0600

    adding files

--- p5/Week8/MP2/sketch.js
+++ p5/Week8/MP2/sketch.js
@@ -6,6 +6,7 @@ var clap;
 function preload(){
   //gif = loadGif('assets/giphybrain1.webp');
   kick = loadSound('assets/kick1.wav');
+  clap = loadSound('kickclap1.wav')
   kick.loop();
   kick.stop();
   clap.loop();
@@ -24,7 +25,7 @@ function draw() {
   switch(myState) {
     case 0:
     background(random(0,255),random(0,0),random(0,255));
-    fill('Black')
+    fill('white')
     text("House music Is a feeling!", 500, 500);
     textAlign(CENTER, CENTER);
     textSize(28);
@@ -33,12 +34,10 @@ function draw() {
     if (timer > 250) {
       myState = 1;
       timer = 0;
+}
     break ;
 
     case 1:
-    break ;
-
-    case 2:
     //background(random(0,255),random(0,0),random(0,255));
     background('black')
     fill('white')
@@ -46,12 +45,11 @@ function draw() {
     textAlign(CENTER, CENTER);
     textSize(28)
     kick.play()
+
+
     break ;
 
-    case 3:
-    break ;
-
-    case 4:
+    case 2:
     background(random(0,255),random(0,0),random(0,255));
     fill('white')
     text("House music Is a feeling!", 500, 500);
@@ -60,20 +58,45 @@ function draw() {
     clap.play()
     break ;
 
+    case 3:
+    background(random(0,255),random(0,0),random(0,255));
+    fill('white')
+    text("House music Is a feeling!", 500, 500);
+    textAlign(CENTER, CENTER);
+    textSize(28)
+    break ;
+
+    case 4:
+    background(random(0,255),random(0,0),random(0,255));
+    fill('white')
+    text("House music Is a feeling!", 500, 500);
+    textAlign(CENTER, CENTER);
+    textSize(28)
+    break ;
+
+    case 5:
+    background(random(0,255),random(0,0),random(0,255));
+    fill('white')
+    text("House music Is a feeling!", 500, 500);
+    textAlign(CENTER, CENTER);
+    textSize(28)
+    break ;
   }
 
 }
 
 function mouseReleased() {
-  if (myState == 0) {
-    myState = 1 ;
+  myState = myState + 1;
+  if (myState > 6) {
+    myState = 0;
   }
 
-  if (myState == 2) {
-    myState = 3 ;
-  }
+  song1.pause();
+  song2.pause();
+  song3.pause();
 
-  //console for mouse position
-  //text(mouseX + ",  " + mouseY, mouseX + 10, mouseY + 10);
+}
 
+function touchStarted() {
+  getAudioContext().resume();
 }
