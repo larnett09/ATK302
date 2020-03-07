8e123e7568c15f383061a0374bdcefb1fed104ff
larnett09 <larnett.br@outlook.com>
2020-03-06 19:11:32 -0600

    I HATE THIS PROJECT

--- p5/Week8/MP2/sketch.js
+++ p5/Week8/MP2/sketch.js
@@ -1,17 +1,16 @@
-var myState = 0 ;
-var timer = 0 ;
-var kick, clap;
-var osc1;
-var osc1amp
-var fft
-var oct
-var isPlaying
-var freq
-var keyIndex
-var pentaTable
+var myState = 0;
+var timer = 0;
+var kick, clap, hats, snare;
+var osc;
+var oscamp;
+var fft;
+var oct;
+var isPlaying;
+var freq;
+var keyIndex;
 
-function preload(){
-  //gif = loadGif('assets/giphybrain1.webp');
+
+function preload() {
   kick = loadSound('assets/kick1.mp3');
   clap = loadSound('assets/kickclap1.mp3');
   kick.loop();
@@ -23,108 +22,111 @@ function preload(){
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  background("white");
+  background("Black");
   frameRate(1);
 
-  pentaTable = [55.0, 65.4064, 73.4162, 77.7817, 82.4069, 97.9989]
-  oct = 0
-  keyIndex = 0
-  freq = 440
-  isPlaying = false
-  osc1amp = 0.0
-  osc1 = new p5.Oscillator()
-  fft = new p5.FFT()
-  osc1.setType('sawtooth')
-  osc1.freq(freq)
-  osc1.amp(0)
-  osc1.start(
+
 }
 
 function draw() {
   // put drawing code here
 
-  switch(myState) {
+  switch (myState) {
     case 0:
-    background(random(0,255),random(0,0),random(0,255));
-    fill('white')
-    text("House music is a feeling!", 500, 500);
-    textAlign(CENTER, CENTER);
-    textSize(28);
+      background('black');
+      textSize(42);
+      fill('white')
+      text("WARNING FLASHING LIGHTS MAY CAUSE SIEZURES", 500, 500);
+      textAlign(CENTER, CENTER);
+      textSize(42);
 
-    timer++;
-    if (timer > 250) {
-      myState = 1;
-      timer = 0;
-}
-    break ;
+      break;
 
     case 1:
-    //background(random(0,255),random(0,0),random(0,255));
-    background('black')
-    fill('white')
-    text("House music gets you dancing.", 500, 500);
-    textAlign(CENTER, CENTER);
-    textSize(28)
-    kick.play()
+      //background(random(0,255),random(0,0),random(0,255));
+      background('black')
+      textSize(42);
+      fill(random(0, 255), random(0, 0), random(0, 255))
+      text("House music gets you dancing.", 500, 500);
+      textAlign(CENTER, CENTER);
+      textSize(28);
+      kick.play();
 
 
-    break ;
+      break;
 
     case 2:
-    background(random(0,255),random(0,0),random(0,255));
-    fill('white')
-    text("House music you feel in your soul!", 500, 500);
-    textAlign(CENTER, CENTER);
-    textSize(28)
-    clap.play()
-    break ;
+      background('Black');
+      textSize(42);
+      fill(random(0, 255), random(0, 0), random(0, 255));
+      text("House music you feel in your soul!", 500, 500);
+      textAlign(CENTER, CENTER);
+      clap.play();
+      break;
 
     case 3:
-    background(random(0,255),random(0,0),random(0,255));
-    fill('white')
-    text("This is your brain on house music!", 500, 500);
-    textAlign(CENTER, CENTER);
-    textSize(28)
-    break ;
+      background('Black');
+      textSize(42);
+      fill(random(0, 255), random(0, 0), random(0, 255));
+      text("This is your brain on house music!", 500, 500);
+      textAlign(CENTER, CENTER);
+      break;
 
     case 4:
-    background(random(0,255),random(0,0),random(0,255));
-    fill('white')
-    text("House music Is a feeling!", 500, 500);
-    textAlign(CENTER, CENTER);
-    textSize(28)
+      background(random(0, 255), random(0, 0), random(0, 255));
+      textSize(42);
+      fill('white');
+      text("House music Is a feeling!", 500, 500);
+      textAlign(CENTER, CENTER);
 
-    playFreq()
-    text("oct : " + oct, 10, 10)
-    text(keyIndex, 50, 10)
-    text("Hz : " + pentaTable[keyIndex] * pow(2, oct), 10, 20)
-    text("sawtooth : " + int(osc2amp * 10), width / 4, height - 30)
-    var waveform = fft.waveform()
-    beginShape()
-    strokeWeight(2)
-    for (var i = 0; i < waveform.length; i++){
-    var x = map(i * 5, 0, waveform.length, 0, width);
-    var y = map(waveform[i], -1, 2, height/10 * 8, 0);
-    vertex(x, y);
- }
- endShape();
-}
 
-    break ;
+      osc = new p5.Oscillator();
+      osc.setType('sawtooth');
+      osc.start();
+      playFreq();
+      text("oct : " + oct, 10, 10);
+      text(keyIndex, 50, 10);
+      text("sawtooth : " + int(oscamp * 10), width / 4, height - 30);
+      var waveform = fft.waveform();
+      beginShape();
+      strokeWeight(2);
 
+      //Still mising something here??
+      for (var i = 0; i < waveform.length; i++) {
+        var x = map(i * 5, 0, waveform.length, 0, width);
+        var y = map(waveform[i], -1, 2, height / 10 * 8, 0);
+        vertex(x, y);
+      }
   }
+  endShape();
+
+
+}
 
 
 function mouseReleased() {
-  if (myState == 0) {
-    myState = 1 ;
+  myState = myState + 0;
+  if (myState > 5) {
+    myState = 0;
   }
 }
-  if (myState == 1) {
-    myState = 2 ;
-  }
-}
-  if (myState == 2) {
-    myState = 3 ;
 
+function mouseMoved() {
+  oct = int(map(height - mouseY, 0, height, 0, 48) / 6)
+  keyIndex = int(map(height - mouseY, 0, height, 0, 48) % 6)
+  playFreq()
+}
+
+function playFreq() {
+  if (keyIndex > 5) {
+    keyIndex = 0
+    oct++
+  } else if (keyIndex < 0) {
+    keyIndex = 5
+    oct--
+  }
+
+  function oscPlay() {
+    osc.stop()
+  }
 }
