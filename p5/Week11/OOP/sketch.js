9febff8e53e3338c88ff0da38c6fb5cba6980485
larnett09 <larnett.br@outlook.com>
2020-03-29 17:46:32 -0500

    10 cars

--- p5/Week11/OOP/sketch.js
+++ p5/Week11/OOP/sketch.js
@@ -1,32 +1,51 @@
-var myCar ;
-var myCar2 ;
-var myCar3 ;
+var cars = [] ;
+//var myCar ;
+//var myCar2 ;
+//var myCar3 ;
 
 function setup() {
   createCanvas(800, 800) ;
-  myCar = new Car() ;
-  myCar2 = new Car() ;
-  myCar3 = new Car() ;
+//  myCar = new Car() ;
+//  myCar2 = new Car() ;
+//  myCar3 = new Car() ;
+
+for (var i = 0 ; i < 10 ; i++) {
+    cars.push(new Car()) ;
+  }
+
 }
 
 function draw() {
   background('black') ;
 
-  myCar.display() ;
-  myCar.drive() ;
+  //myCar.display() ;
+  //myCar.drive() ;
 
-  myCar2.display() ;
-  myCar2.drive() ;
+  //myCar2.display() ;
+  //myCar2.drive() ;
 
-  myCar3.display() ;
-  myCar3.drive() ;
+  //myCar3.display() ;
+  //myCar3.drive() ;
+
+  for (var i = 0 ; i < 10 ; i++) {
+    cars[i].display() ;
+    cars[i].display() ;
+  }
+
 }
 
 function Car() {
+  //atributes
   this.pos = createVector(random(width), random(height)) ;
-  this.vel createVector(random(-5,5), random(-5,5)) ;
+  this.vel = createVector(random(-5,5), random(-5,5)) ;
+  this.r = random(255) ;
+  this.g = random(255) ;
+  this.b = random(255) ;
 
+
+  //methods
   this.display = function() {
+    fill(this.r, this.g, this.b) ;
     rect(this.pos.x, this.pos.y, 100, 100) ;
   }
 
