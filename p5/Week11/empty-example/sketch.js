cc4027554281d26bf6a4b5a743c181883186b0bf
larnett09 <larnett.br@outlook.com>
2020-03-29 16:59:27 -0500

    created week 11

---
+++ p5/Week11/empty-example/sketch.js
@@ -0,0 +1,28 @@
+var = myCar ;
+
+
+function setup() {
+  createCanvas(800, 800) ;
+  myCar = new Car() ;
+}
+
+function draw() {
+  background('black') ;
+
+
+
+
+}
+
+function Car() {
+  this.x = 100 ;
+
+  this.display = function() {
+    rect(this.x, 100, 100, 100) ;
+  }
+
+  this.drive = function() {
+    this.x = this.x + 5 ;
+  }
+
+}
