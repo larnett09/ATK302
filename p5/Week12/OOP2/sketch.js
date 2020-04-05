ca245546d0b80bf773cffacf2bbddf22bb0c57d3
larnett09 <larnett.br@outlook.com>
2020-04-05 16:52:33 -0500

    cars change

--- p5/Week12/OOP2/sketch.js
+++ p5/Week12/OOP2/sketch.js
@@ -73,33 +73,28 @@ function mouseReleased() {
   }
 }
 
-// car class!!
 function Car() {
-  // attributes
-  this.pos = createVector(100, 100);
-  this.vel = createVector(random(-5, 5), random(-5, 5));
-  this.r = random(255);
-  this.g = random(255);
-  this.b = random(255);
+  //atributes
+  this.pos = createVector(random(width), random(height)) ;
+  this.vel = createVector(random(-5,5), random(-5,5)) ;
+  this.r = random(255) ;
+  this.g = random(255) ;
+  this.b = random(255) ;
 
 
-  // methods
+  //methods
   this.display = function() {
-    fill(this.r, this.g, this.b);
-    rect(this.pos.x, this.pos.y, 100, 50);
-    ellipse(this.pos.x - 45, this.pos.y + 25, 50, 50);
-    ellipse(this.pos.x + 45, this.pos.y + 25, 50, 50);
+    fill(this.r, this.g, this.b) ;
+    rect(this.pos.x, this.pos.y, 30, 30) ;
   }
 
   this.drive = function() {
-    this.pos.add(this.vel);
-
-    if (this.pos.x > width) this.pos.x = 0;
-    if (this.pos.x < 0) this.pos.x = width;
-    if (this.pos.y > height) this.pos.y = 0;
-    if (this.pos.y < 0) this.pos.y = height;
-
-  }
+    this.pos.add(this.vel)  ;
+    if (this.pos.x > width) this.pos.x = 0 ;
+    if (this.pos.x <0) this.pos.x = width ;
+    if (this.pos.y > height) this.pos.y = 0 ;
+    if (this.pos.y <0) this.pos.y = height ;
+    }
 
 }
 
