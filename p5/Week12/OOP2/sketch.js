869744308407356dd0f6fbbae6801b667de881e1
larnett09 <larnett.br@outlook.com>
2020-04-12 15:09:15 -0500

    new path

--- p5/Week12/OOP2/sketch.js
+++ p5/Week12/OOP2/sketch.js
@@ -2,7 +2,8 @@ var cars = [] ;
 var frogPos ;
 var myState = 0;
 var timer = 0 ;
-var maxCars = 10 ;
+var maxCars = 40 ;
+var frog ;
 
 function setup() {
 
@@ -17,8 +18,8 @@ function setup() {
   frogPos = createVector(400, height - 100) ;
 
   textAlign(CENTER)
-  //rectMode(CENTER);
-  //ellipseMode(CENTER);
+  imageMode(CENTER);
+  frog = loadImage('/assets/frog.png')
 }
 
 function draw() {
@@ -143,7 +144,8 @@ function game() {
   }
 
   // draw the frog
-  fill('green');
-  ellipse(frogPos.x, frogPos.y, 60, 60);
-  checkForKeys();
+  //fill('green');
+  //ellipse(frogPos.x, frogPos.y, 60, 60);
+  image(frog, frogPos.x, frogPos.y) ;
+  checkForKeys() ;
 }
