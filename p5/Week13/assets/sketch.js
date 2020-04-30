de607c11ee6d1d38ab3aba936e5820e8757f5362
larnett09 <larnett.br@outlook.com>
2020-04-30 10:27:09 -0500

    added the new js script testing now

--- p5/Week13/assets/sketch.js
+++ p5/Week13/assets/sketch.js
@@ -6,39 +6,23 @@ var maxCars = 20 ;
 var frog ;
 var fly ;
 var bg ;
-var alpha, beta, gamma; // orientation data
-var bunnyImage;
-var xPosition = 0;
-var yPosition = 0;
-var x = 0; // acceleration data
-var y = 0;
-var z = 0;
-
+var buzz ;
 
 function preload() {
-  
+  buzz = loadSound('https://larnett09.github.io/ATK302/p5/Week13/assets/buzz.wav') ;
 }
 
 function setup() {
 
-  createCanvas(windowWidth, windowHeight);
-
-   // initialize accelerometer variables
-   alpha = 0;
-   beta = 0;
-   gamma = 0;
- 
-   bunnyImage = loadImage("assets/.jpg");
-   imageMode(CENTER);
-   rectMode(CENTER);
+  createCanvas(800, 800) ;
 
   // spawn cars!!!
   for (var i = 0; i < 5; i++) {
     cars.push(new Car()) ;
   }
 
-  frogPos = createVector(400, height - 100) ;
 
+  frogPos = createVector(400, height - 100) ;
 
   textAlign(CENTER) ;
   imageMode(CENTER) ;
@@ -49,48 +33,6 @@ function setup() {
 }
 
 function draw() {
-  background('black'); //
-
-  // the map command !!!!
-  // takes your variable and maps it from range 1 to range 2
-  // map(yourVar, range1_x, range1_y, range2_x, range2_y) ;
-  xPosition = map(gamma, -60, 60, 0, width);
-  yPosition = map(beta, -30, 30, 0, height);
-
-  push(); // before you use translate, rotate, or scale commands, push and then pop after
-
-  translate(xPosition, yPosition); // move everything over by x, y
-
-  rotate(radians(alpha)); // using alpha in here so it doesn't feel bad
-
-  image(bunnyImage, 0, 0, 500, 500);
-  //  	rect(0, 0, 100, 100) ;
-  pop();
-
-
-  // DECORATIONS
-  // Just a bunch of text commands to display data coming in from addEventListeners
-  textAlign(LEFT);
-  textSize(20);
-  fill('white');
-  text("orientation data:", 25, 25);
-  textSize(15);
-  text("alpha: " + alpha, 25, 50);
-  text("beta: " + beta, 25, 70);
-  text("gamma: " + gamma, 25, 90);
-  textSize(20);
-  text("acceleration data:", 25, 125);
-  textSize(15);
-  text("x = " + x.toFixed(2), 25, 150); // .toFixed means just show (x) decimal places
-  text("y = " + y.toFixed(2), 25, 170);
-  text("z = " + z.toFixed(4), 25, 190);
-
-  // MORE DECORATIONS - write that pretty ATK type on top.
-  fill('white');
-  noStroke();
-  textSize(300);
-  textAlign(CENTER);
-  text("atk", width / 2, height / 2);
 
   switch (myState) {
 
--- p5/Week13/mp3/sketch.js
+++ p5/Week13/mp3/sketch.js
@@ -6,23 +6,39 @@ var maxCars = 20 ;
 var frog ;
 var fly ;
 var bg ;
-var buzz ;
+var alpha, beta, gamma; // orientation data
+var bunnyImage;
+var xPosition = 0;
+var yPosition = 0;
+var x = 0; // acceleration data
+var y = 0;
+var z = 0;
+
 
 function preload() {
-  buzz = loadSound('https://larnett09.github.io/ATK302/p5/Week13/assets/buzz.wav') ;
+  
 }
 
 function setup() {
 
-  createCanvas(800, 800) ;
+  createCanvas(windowWidth, windowHeight);
+
+   // initialize accelerometer variables
+   alpha = 0;
+   beta = 0;
+   gamma = 0;
+ 
+   bunnyImage = loadImage("assets/.jpg");
+   imageMode(CENTER);
+   rectMode(CENTER);
 
   // spawn cars!!!
   for (var i = 0; i < 5; i++) {
     cars.push(new Car()) ;
   }
 
-
   frogPos = createVector(400, height - 100) ;
+
 
   textAlign(CENTER) ;
   imageMode(CENTER) ;
@@ -33,6 +49,48 @@ function setup() {
 }
 
 function draw() {
+  background('black'); //
+
+  // the map command !!!!
+  // takes your variable and maps it from range 1 to range 2
+  // map(yourVar, range1_x, range1_y, range2_x, range2_y) ;
+  xPosition = map(gamma, -60, 60, 0, width);
+  yPosition = map(beta, -30, 30, 0, height);
+
+  push(); // before you use translate, rotate, or scale commands, push and then pop after
+
+  translate(xPosition, yPosition); // move everything over by x, y
+
+  rotate(radians(alpha)); // using alpha in here so it doesn't feel bad
+
+  image(bunnyImage, 0, 0, 500, 500);
+  //  	rect(0, 0, 100, 100) ;
+  pop();
+
+
+  // DECORATIONS
+  // Just a bunch of text commands to display data coming in from addEventListeners
+  textAlign(LEFT);
+  textSize(20);
+  fill('white');
+  text("orientation data:", 25, 25);
+  textSize(15);
+  text("alpha: " + alpha, 25, 50);
+  text("beta: " + beta, 25, 70);
+  text("gamma: " + gamma, 25, 90);
+  textSize(20);
+  text("acceleration data:", 25, 125);
+  textSize(15);
+  text("x = " + x.toFixed(2), 25, 150); // .toFixed means just show (x) decimal places
+  text("y = " + y.toFixed(2), 25, 170);
+  text("z = " + z.toFixed(4), 25, 190);
+
+  // MORE DECORATIONS - write that pretty ATK type on top.
+  fill('white');
+  noStroke();
+  textSize(300);
+  textAlign(CENTER);
+  text("atk", width / 2, height / 2);
 
   switch (myState) {
 
