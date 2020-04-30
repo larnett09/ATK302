495a6e4108fc5855fb51ad5b701e4a7957e143c5
larnett09 <larnett.br@outlook.com>
2020-04-30 13:40:51 -0500

    new image

--- p5/mp4/weather/sketch.js
+++ p5/mp4/weather/sketch.js
@@ -2,28 +2,36 @@ var weather;
 var temp = 0;
 var weatherID = 0; // returned in the JSON weather element
 var myState = 0;
-var bg
-var sun
-var cloud
+var bg ;
+var sun ;
+var cloud ;
+var x = 0 ;
+var ws = 0 ;
 
+function preload() {
+  bg = loadImage('https://larnett09.github.io/ATK302/p5/mp4/weather/assets/bg.png') ;
+  cloud = loadImage('https://larnett09.github.io/ATK302/p5/mp4/weather/assets/cloud.png') ;
+  sun = loadImage('https://larnett09.github.io/ATK302/p5/mp4/weather/assets/sun.png') ;
+}
 
 function setup() {
-  createCanvas(400, 400);
-
+  createCanvas(400, 400) ;
+  image(bg, 400, 400, 400, 400) ;  
   // HERE Is the call to get the weather. PLEASE PUT YOUR OWN ID IN HERE!!!
   // MINE WILL STOP WORKING IF EVERYONE USES IT!!!
-  bg = loadImage('https://larnett09.github.io/ATK302/p5/mp4/weather/assets/bg.png') ;
 
+
+  imageMode(CENTER) ;
   var myJSONString = 'https://api.openweathermap.org/data/2.5/weather?q=Vancouver, us&units=imperial&';
   var myIDString = 'appid=06c501c724291e7f017446666447cc76'; // Take out the x’s and ADD YOUR ID!!!
-  loadJSON(myJSONString + myIDString, gotData); // look - that gotData function happens when JSON comes back.
-
+  loadJSON(myJSONString + myIDString, gotData); // look - that gotData function happens when JSON comes back.  
 }
 
 
 function gotData(data) {
-  weather = data;
-  console.log(data); // for debugging purposes, print out the JSON data when we get it.
+  weather = data ;
+  console.log(data) ; // for debugging purposes, print out the JSON data when we get it.
+  ws = weather.wind.speed ;  
 }
 
 
@@ -36,15 +44,18 @@ function draw() {
       break;
 
     case 1:
-    image(bg, 400, 400, 800, 800) ;
+    image(bg, 400, 400, 400, 400) ;    
     fill('black') ;
     text("Lovely Weather in Vancouver", 20, 20) ;
 // parse the weather object and put some text or images using at least 3 different weather data!
     text("The Current Forcast is"+ weather.weather.description, 20, 40);
-    text("The Temperature is"+ weather.main.temp, 40, 40);
-    text("The Humidity is"+ weather.main.humidity, 60, 40);
-    text("The Windspeed is"+ weather.wind.speed, 80, 40);
-    
+    text("The Temperature is"+ weather.main.temp, 20, 60);
+    text("The Humidity is"+ weather.main.humidity, 20, 80);
+    text("The Windspeed is"+ weather.wind.speed, 20, 100);
+    image(cloud, x, 250, 200, 100);
+    x = x + ws/2 ;
+    if (x > width) x = 0 ;
+
     break;
 
   }
